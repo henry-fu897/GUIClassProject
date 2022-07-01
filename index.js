866031de08f04b0d83601c232fc74131d79bf78d
henryfu897 <henryfu897@vt.edu>
2022-07-01 16:35:30 -0400

    added proxies to packages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 const app = express()
 //const publicPath = path.join(__dirname, 'build')
