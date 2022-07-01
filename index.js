9c50ee2112bc6be021dffba1f32bcce6eda44898
henryfu897 <henryfu897@vt.edu>
2022-07-01 15:33:13 -0400

    making the tabs work

--- index.js
+++ index.js
@@ -28,3 +28,18 @@ app.listen(port, (err)=>{
     }
     console.log(`Server Running. Listening on port ${port}`)
 })
+
+function openTab(current, tabName) {
+    /*remove the class active  */
+    for(item of document.getElementsByClassName("tabbutton")) {
+        item.className.replace("active", "");
+    }
+    /*hide all other tabcontent  */
+    for(item of document.getElementsByClassName("tabcontent")) {
+        item.style.display = "none"
+    }
+
+    /*show current tab and add active to the button */
+    document.getElementById(tabName).style.display = "block";
+    current.currentTarget.className += " active";
+}
