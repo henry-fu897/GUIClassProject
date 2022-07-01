09ae75c7dc96daf4af562366b2d48c5210f3d8cf
henryfu897 <henryfu897@vt.edu>
2022-07-01 19:07:19 -0400

    hw4 updated

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -13,7 +13,7 @@ const p = d3.json('finance.json').then(function(data) {
         .domain([0, 64])
         .range([0, width - spacing]);
     var yScale = d3.scaleLinear()
-        .domain([3000, 0])
+        .domain([d3.max(data.chart.result[0].comparisons[1].high), 0])
         .range([0, height - spacing]);
 
     var xAxis = d3.axisBottom(xScale)      
@@ -46,70 +46,81 @@ const p = d3.json('finance.json').then(function(data) {
         .enter().append("circle")
         .attr("cx", (p, i) => {return xScale(i)})
         .attr("cy", (p, i) => {return yScale(p)})
-        .attr('r', 2).attr('fill', 'red')
+        .attr('r', 3).attr('fill', 'red')
 
     svg.append("g")
         .selectAll("dot").data(data.chart.result[0].comparisons[1].high)
         .enter().append("circle")
         .attr("cx", (p, i) => {return xScale(i)})
         .attr("cy", (p, i) => {return yScale(p)})
-        .attr('r', 2).attr('fill', 'blue')
+        .attr('r', 3).attr('fill', 'blue')
 
     svg.append("g")
         .selectAll("dot").data(data.chart.result[0].comparisons[2].high)
         .enter().append("circle")
         .attr("cx", (p, i) => {return xScale(i)})
         .attr("cy", (p, i) => {return yScale(p)})
-        .attr('r', 2).attr('fill', 'yellow')
+        .attr('r', 3)
+        .attr('fill', 'yellow')
 
-    =svg.append("g")
+    svg.append("g")
         .selectAll("dot").data(data.chart.result[0].comparisons[3].high)
         .enter().append("circle")
         .attr("cx", (p, i) => {return xScale(i)})
         .attr("cy", (p, i) => {return yScale(p)})
-        .attr('r', 2).attr('fill', 'green');
+        .attr('r', 3).attr('fill', 'green');
+
+    
     
     //Dot Information
+    d3.selectAll("#Legend")
+        .append("div")
+        .attr("id","tooltip")
+        .style("position", "absolute")
+        .text("preview data");
 
+    //
+    svg.selectAll("circle")
+        .on("mousemove", function(){d3.selectAll("#tooltip").text(this.__data__)})
     
     
     //Legend
     //Meta
-    svg.selectAll("body")
+    svg.selectAll("#Legend")
         .data("Meta").enter().append('rect')
         .attr('x', 820).attr('y', 50).attr('width', 10)
         .attr('height', 10).attr('fill', 'red')
-    svg.selectAll("body")
+    svg.selectAll("#Legend")
         .data("Meta").enter().append('text')
         .attr('x', 835).attr('y', 60)
         .attr('width', 10).attr('height', 10)
         .text("Meta").attr('fill', 'red')
     //Google
-    svg.selectAll("body")
+    svg.selectAll("Legend")
         .data("Google").enter()
         .append('rect').attr('x', 820).attr('y', 70)
         .attr('width', 10).attr('height', 10).attr('fill', 'blue')
-    svg.selectAll("body")
+    svg.selectAll("Legend")
         .data("Google").enter()
         .append('text').attr('x', 835).attr('y', 80)
         .attr('width', 10).attr('height', 10)
         .text("Google").attr('fill', 'blue')
     //Tesla
-    svg.selectAll("body")
+    svg.selectAll("#Legend")
             .data("Tesla").enter()
             .append('rect').attr('x', 820).attr('y', 90)
             .attr('width', 10).attr('height', 10).attr('fill', 'yellow')    
-    svg.selectAll("body")
+    svg.selectAll("#Legend")
         .data("Tesla").enter().append('text')
             .attr('x', 835).attr('y', 100)
             .attr('width', 10).attr('height', 10)
             .text("Tesla").attr('fill', 'yellow')
     //amazon
-    svg.selectAll("body")
+    svg.selectAll("Legend")
         .data("Amazon").enter().append('rect')
         .attr('x', 820).attr('y', 110)
         .attr('width', 10).attr('height', 10).attr('fill', 'green')
-    svg.selectAll("body")
+    svg.selectAll("#Legend")
         .data("Amazon").enter().append('text')
         .attr('x', 835).attr('y', 120)
         .attr('width', 10).attr('height', 10)
